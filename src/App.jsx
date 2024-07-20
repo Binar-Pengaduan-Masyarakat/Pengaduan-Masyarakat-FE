1411aa94a50f023b0c4718163b49f1d185a99ede
Rasyahodhe <rasyahodhe450@gmail.com>
2024-07-20 11:18:24 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,8 @@
-<<<<<<< HEAD
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import ReportsTable from "./components/reportsTable";
-import ReportDetailsPage from "./components/reportDetails";
-import { Container } from "react-bootstrap";
-import UserProfile from "./components/userProfile";
-import Header from "./components/header";
-import "../public/css/public.css";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Container>
-        <Routes>
-          <Route path="/" element={<ReportsTable />} />
-          <Route
-            path="/report-details/:reportId"
-            element={<ReportDetailsPage />}
-          />
-          <Route path="/users/:userId" element={<UserProfile />} />
-        </Routes>
-      </Container>
-    </BrowserRouter>
-  );
-=======
 import "./App.css";
 import Routers from "./components/Router";
 
 function App() {
   return <Routers />;
->>>>>>> fe-reports
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-<<<<<<< HEAD
-import { UserProvider } from "./components/userContext";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
-=======
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
->>>>>>> fe-reports
   </React.StrictMode>
 );
