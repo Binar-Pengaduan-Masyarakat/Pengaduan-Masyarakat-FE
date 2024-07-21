524b3b5f7067b6426018b036fb3cee31a1c4cdd9
imfauzan14 <imfauzan14@gmail.com>
2024-07-22 00:52:24 +0700

    fix

--- src/components/page-Super-Admin/Sidebar.jsx
+++ src/components/page-Super-Admin/Sidebar.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import "../css/page-institute/admin.css"
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "../css/page-institute/admin.css";
 // import avatar from '../../assets/image/profile.jpeg';
 
 const SuperAdmin = () => {
-  const [userName, setUserName] = useState('');
+  const [userName, setUserName] = useState("");
   useEffect(() => {
-    const user = JSON.parse(localStorage.getItem('user'));
+    const user = JSON.parse(localStorage.getItem("user"));
     if (user) {
       setUserName(user.name);
     }
@@ -15,10 +15,18 @@ const SuperAdmin = () => {
     <div className="admin-sidebar">
       <nav className="sidebar-nav">
         <ul>
-          <li><Link to="/superadmin/dashboard">Dashboard</Link></li>
-          <li><Link to="/superadmin/managementusers">Management Users</Link></li>
-          <li><Link to="/superadmin/instansiManagement">Management Admin Instansi</Link></li>
-          <li><Link to="/superadmin/reports">Master Data Aduan</Link></li>
+          <li>
+            <Link to="/superadmin/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/superadmin/managementusers">Management Users</Link>
+          </li>
+          <li>
+            <Link to="/superadmin/instansiManagement">Management Admin</Link>
+          </li>
+          <li>
+            <Link to="/superadmin/reports">Master Data Aduan</Link>
+          </li>
         </ul>
       </nav>
     </div>
